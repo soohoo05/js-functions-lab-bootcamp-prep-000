d3e41caaeb5e1f2f0485bc2e4b458881f394ef50
Tyler Soo Hoo <tyler.soohoo@gmail.com>
2018-07-25 20:58:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function happyHolidaysTo(name){
 function happyHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
 }
-function holidayCountdown(days,holiday){
+function holidayCountdown(holiday,name){
   return `It\'s ${days} days until ${holiday}!`
 }
